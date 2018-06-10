31b944d08a908bd6b801d4ec372180396aa5a3d3
swagbrecht <gerhard.lambrecht@edu.fh-joanneum.at>
2018-06-10 19:30:09 +0200

    Removed unused inputs Re

--- src/at/fh/swenga/grazious/model/UserRoleModel.java
+++ src/at/fh/swenga/grazious/model/UserRoleModel.java
@@ -2,14 +2,11 @@ package at.fh.swenga.grazious.model;
 
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.GenerationType;
