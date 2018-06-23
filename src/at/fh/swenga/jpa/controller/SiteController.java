55e29d275e6f0a40abc50cddf793d5694c7e1d10
swagbrecht <gerhard.lambrecht@edu.fh-joanneum.at>
2018-06-23 05:01:25 +0200

    Added Data

--- src/at/fh/swenga/jpa/controller/SiteController.java
+++ src/at/fh/swenga/jpa/controller/SiteController.java
@@ -133,7 +133,23 @@ public class SiteController {
 		// regions
 		RegionModel regionAndritzModel = new RegionModel("Andritz");
 		RegionModel regionPuntigamModel = new RegionModel("Puntigam");
-		
+		RegionModel regionInnereStadtModel = new RegionModel("Innere Stadt");
+		RegionModel regionLeonhardModel = new RegionModel("St. Leonhard");
+		RegionModel regionGeidorfModel = new RegionModel("Geidorf");
+		RegionModel regionLendModel = new RegionModel("Lend");
+		RegionModel regionGriesModel = new RegionModel("Gries");
+		RegionModel regionJakominiModel = new RegionModel("Jakomini");
+		RegionModel regionLiebenauModel = new RegionModel("Liebenau");
+		RegionModel regionPeterModel = new RegionModel("St. Peter");
+		RegionModel regionWaltendorfModel = new RegionModel("Waltendorf");
+		RegionModel regionRiesModel = new RegionModel("Ries");
+		RegionModel regionMariatrostModel = new RegionModel("Mariatrost");
+		RegionModel regionGoestingModel = new RegionModel("Gösting");
+		RegionModel regionEggenbergModel = new RegionModel("Eggenberg");
+		RegionModel regionWetzelsdorfModel = new RegionModel("Wetzelsdorf");
+		RegionModel regionStrassgangModel = new RegionModel("Straßgang");
+
+
 		// user roles
 		UserRoleModel userRoleAdmin = new UserRoleModel("ROLE_ADMIN");
 		UserRoleModel userRoleUser = new UserRoleModel("ROLE_USER");
